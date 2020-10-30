41d0063f98430280c1f90b7186a2603a033ce08b
Patryk Wyprzał <patryk836@o2.pl>
2020-10-30 15:45:01 +0100

    SPA-RefactoringLoginTS

--- TicketManagement-SPA/src/app/components/login/login.component.ts
+++ TicketManagement-SPA/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { retry } from "rxjs/operators";
 import { WrapperService } from "src/app/shared/wrapper.service";
 
 @Component({
@@ -17,18 +18,22 @@ export class LoginComponent implements OnInit {
   login() {
     this.wrapperService.AuthService.login(this.loginModel).subscribe(
       () => {
-        if (
-          this.wrapperService.AuthService.decodedToken.role.indexOf("admin") !==
-          -1
-        ) {
-          this.router.navigate(["admin/issues"]);
-        } else {
-          this.router.navigate(["user/issues"]);
-        }
+        this.NavigateToAppropiatePage();
       },
       (error) => {
         this.wrapperService.ErrorService.newError(error);
       }
     );
+  }
+
+  private NavigateToAppropiatePage() {
+    if (
+      this.wrapperService.AuthService.decodedToken.role.indexOf("admin") !== -1
+    ) {
+      this.router.navigate(["admin/issues"]);
+      return;
+    }
+
+    this.router.navigate(["user/issues"]);
   }
 }
