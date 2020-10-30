0af72eaee19caa20237e1370dffbb1ad07fe1491
Patryk Wyprzał <patryk836@o2.pl>
2020-10-30 14:54:32 +0100

    SPA-RefactoringIssueService

--- TicketManagement-SPA/src/app/components/issues/issue.service.ts
+++ TicketManagement-SPA/src/app/components/issues/issue.service.ts
@@ -22,58 +22,7 @@ export class IssueService {
   getIssues(
     searchSpecification: SearchSpecificationModel
   ): Observable<PaginatedItemsModel> {
-    let params = new HttpParams();
-
-    if (searchSpecification.declarantLastName != null) {
-      params = params.append(
-        "declarantLastName",
-        searchSpecification.declarantLastName
-      );
-    }
-
-    if (searchSpecification.departament != null) {
-      params = params.append("departament", searchSpecification.departament);
-    }
-
-    if (searchSpecification.status != null) {
-      params = params.append("status", searchSpecification.status.toString());
-    }
-
-    if (searchSpecification.priority != null) {
-      params = params.append(
-        "priority",
-        searchSpecification.priority.toString()
-      );
-    }
-
-    if (searchSpecification.title != null) {
-      params = params.append("title", searchSpecification.title);
-    }
-
-    if (searchSpecification.userId != null) {
-      params = params.append("userId", searchSpecification.userId);
-    }
-
-    if (searchSpecification.pageIndex != null) {
-      params = params.append(
-        "pageIndex",
-        searchSpecification.pageIndex.toString()
-      );
-    }
-
-    if (searchSpecification.pageSize != null) {
-      params = params.append(
-        "pageSize",
-        searchSpecification.pageSize.toString()
-      );
-    }
-
-    if (searchSpecification.searchFor != null) {
-      params = params.append(
-        "searchFor",
-        searchSpecification.searchFor.toString()
-      );
-    }
+    let params = this.SetHttpParams(searchSpecification);
 
     return this.http
       .get<PaginatedItemsModel>(this.baseUrl, { params })
@@ -133,5 +82,64 @@ export class IssueService {
     return this.http.get<IssueSupportModel[]>(
       this.baseUrl + issueId + "/support"
     );
+  }
+
+  private SetHttpParams(
+    searchSpecification: SearchSpecificationModel
+  ): HttpParams {
+    let params = new HttpParams();
+
+    if (searchSpecification.declarantLastName != null) {
+      params = params.append(
+        "declarantLastName",
+        searchSpecification.declarantLastName
+      );
+    }
+
+    if (searchSpecification.departament != null) {
+      params = params.append("departament", searchSpecification.departament);
+    }
+
+    if (searchSpecification.status != null) {
+      params = params.append("status", searchSpecification.status.toString());
+    }
+
+    if (searchSpecification.priority != null) {
+      params = params.append(
+        "priority",
+        searchSpecification.priority.toString()
+      );
+    }
+
+    if (searchSpecification.title != null) {
+      params = params.append("title", searchSpecification.title);
+    }
+
+    if (searchSpecification.userId != null) {
+      params = params.append("userId", searchSpecification.userId);
+    }
+
+    if (searchSpecification.pageIndex != null) {
+      params = params.append(
+        "pageIndex",
+        searchSpecification.pageIndex.toString()
+      );
+    }
+
+    if (searchSpecification.pageSize != null) {
+      params = params.append(
+        "pageSize",
+        searchSpecification.pageSize.toString()
+      );
+    }
+
+    if (searchSpecification.searchFor != null) {
+      params = params.append(
+        "searchFor",
+        searchSpecification.searchFor.toString()
+      );
+    }
+
+    return params;
   }
 }
