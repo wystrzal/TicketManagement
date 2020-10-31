c4d4dc8aadca70d776a26c04bc8d6793db6d5029
Patryk Wyprzał <patryk836@o2.pl>
2020-10-31 13:30:09 +0100

    API-RefactoringTokenService

--- TicketManagement.API/Infrastructure/Services/TokenService.cs
+++ TicketManagement.API/Infrastructure/Services/TokenService.cs
@@ -25,43 +25,54 @@ namespace TicketManagement.API.Infrastructure.Services
         public async Task<string> GenerateToken(User user, UserManager<User> userManager)
         {
             var currentUser = await userManager.FindByIdAsync(user.Id);
-            var userClaim = await userManager.GetClaimsAsync(currentUser);
-            var roles = userClaim.Where(c => c.Type == ClaimTypes.Role).ToList();
+            var userClaims = await userManager.GetClaimsAsync(currentUser);
+            var claims = CreateClaims(userClaims, user);
+
+            var tokenDescriptor = CreateSecurityTokenDescriptor(claims);
+
+            return CreateTokenModel(tokenDescriptor);
+        }
+
+        private List<Claim> CreateClaims(IList<Claim> userClaims, User user)
+        {
+            var roles = userClaims.Where(c => c.Type == ClaimTypes.Role).ToList();
 
             var claims = new List<Claim>
             {
-              new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-              new Claim(ClaimTypes.Name, user.UserName)
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName)
             };
 
-            var roleClaims = new List<string>();
-
-            //Add role claims for user.
             if (roles.Count() > 0 && roles != null)
             {
                 foreach (var role in roles)
                 {
                     claims.Add(new Claim(ClaimTypes.Role, role.Value));
-                    roleClaims.Add(role.Value);
                 }
             }
 
+            return claims;
+        }
+
+        private SecurityTokenDescriptor CreateSecurityTokenDescriptor(List<Claim> claims)
+        {
             var key = new SymmetricSecurityKey(Encoding.UTF8
-            .GetBytes(configuration.GetSection("AppSettings:Token").Value));
+                .GetBytes(configuration.GetSection("AppSettings:Token").Value));
 
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-            var tokenDescriptor = new SecurityTokenDescriptor
+            return new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.Now.AddDays(1),
                 SigningCredentials = creds
             };
+        }
 
+        private string CreateTokenModel(SecurityTokenDescriptor tokenDescriptor)
+        {
             var tokenHandler = new JwtSecurityTokenHandler();
-
             var token = tokenHandler.CreateToken(tokenDescriptor);
-
             return tokenHandler.WriteToken(token);
         }
     }
